d4bbad7e1623139cf5ef287006967a66026b315d
git@pierrefhapp.dev <git@pierrefhapp.dev>
2022-01-29 00:20:54 +0100

    create router with /todos prefix which enables us to group our routes and remove the prefix from our /routes/todos file.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import express from "express"
-import {Todo} from "./types/todos"
-import {v4 as uuidv4} from "uuid"
+import todosRouter from "./routes/todos"
 
 // creates an express app.
 const app = express()
@@ -9,72 +8,8 @@ const app = express()
 // request.
 app.use(express.json())
 
-// this is our mock db for now.  will be reset every time we change and save the
-// file.. but its enough for testing express out. :D
-const todos: Todo[] = []
-
-// defines a route
-// get --> HTTP METHOD GET
-app.get("/todos", (_, res) => {
-    return res.json(todos)
-})
-
-// gets a single todo based on uuid.
-app.get("/todos/:uuid", (req, res) => {
-    // desctructure the uuid
-    const {uuid} = req.params
-    const todo = todos.find((todo) => todo.uuid === uuid)
-    return res.json(todo)
-})
-
-// updates a single todo.
-app.put("/todos/:uuid", (req, res) => {
-    const {uuid} = req.params
-    const {title, completed} = req.body
-
-    // error out if required data was not given.
-    if (!title || !completed) {
-        res.send(500)
-    }
-    const index = todos.findIndex((todo) => todo.uuid === uuid)
-    // error out if todo was not found.
-    if (index === -1) {
-        res.send(500)
-    }
-    const updatedTodo = {title, completed, uuid}
-    todos[index] = updatedTodo
-    res.json(updatedTodo)
-})
-
-// post --> HTTP METHOD POST. used to create data for a given entity.
-app.post("/todos", (req, res) => {
-    const {title} = req.body
-    // error out if empty title.
-    if (!title) {
-        res.send(500);
-    }
-    const todo: Todo = {
-        uuid: uuidv4(),
-        title,
-        completed: false
-    }
-    todos.push(todo);
-    // res.send(200);
-    res.json(todo);
-})
-
-// delete --> HTTP METHOD DELETE. used to delete data from a given entitity.
-app.delete("/todos/:uuid", (req, res) => {
-    const {uuid} = req.params
-    const index = todos.findIndex((todo) => todo.uuid === uuid)
-    // error out if todo was not found.
-    if (index === -1) {
-        res.send(500)
-    }
-    const deletedTodo = todos[index]
-    todos.splice(index, 1)
-    res.json(deletedTodo)
-})
+// app.use("", todosRouter)
+app.use("/todos", todosRouter)
 
 // starting the serve and listening on port 3001.
 app.listen(3001, () => console.log("Listening on port 3001!"))
---
+++ src/routes/todos.ts
@@ -0,0 +1,78 @@
+import {Router} from "express"
+import {Todo} from "../types/todos"
+import {v4 as uuidv4} from "uuid"
+
+// can use this to group routes
+const router = Router()
+
+// this is our mock db for now.  will be reset every time we change and save the
+// file.. but its enough for testing express out. :D
+const todos: Todo[] = []
+
+// NOTE(pierre): We could remove the /todos/ prefix for each route by using
+// app.use("/todos",routerHere) inside index.ts
+
+// defines a route
+// get --> HTTP METHOD GET
+router.get("", (_, res) => {
+    return res.json(todos)
+})
+
+// gets a single todo based on uuid.
+router.get("/:uuid", (req, res) => {
+    // desctructure the uuid
+    const {uuid} = req.params
+    const todo = todos.find((todo) => todo.uuid === uuid)
+    return res.json(todo)
+})
+
+// updates a single todo.
+router.put("/:uuid", (req, res) => {
+    const {uuid} = req.params
+    const {title, completed} = req.body
+
+    // error out if required data was not given.
+    if (!title || !completed) {
+        res.send(500)
+    }
+    const index = todos.findIndex((todo) => todo.uuid === uuid)
+    // error out if todo was not found.
+    if (index === -1) {
+        res.send(500)
+    }
+    const updatedTodo = {title, completed, uuid}
+    todos[index] = updatedTodo
+    res.json(updatedTodo)
+})
+
+// post --> HTTP METHOD POST. used to create data for a given entity.
+router.post("", (req, res) => {
+    const {title} = req.body
+    // error out if empty title.
+    if (!title) {
+        res.send(500);
+    }
+    const todo: Todo = {
+        uuid: uuidv4(),
+        title,
+        completed: false
+    }
+    todos.push(todo);
+    // res.send(200);
+    res.json(todo);
+})
+
+// delete --> HTTP METHOD DELETE. used to delete data from a given entitity.
+router.delete("/:uuid", (req, res) => {
+    const {uuid} = req.params
+    const index = todos.findIndex((todo) => todo.uuid === uuid)
+    // error out if todo was not found.
+    if (index === -1) {
+        res.send(500)
+    }
+    const deletedTodo = todos[index]
+    todos.splice(index, 1)
+    res.json(deletedTodo)
+})
+
+export default router
